613ef0bb62c1a14910298abffd56720ea1640ea6
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-11-15 11:53:27 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+  setup() {},
+  watch: {
+    $route(to) {
+      document.title = `Smart Visa Bali - ${to.meta.title}`;
+    },
+  },
+  methods: {},
+  created() {},
+};
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Home from "/src/views/landing/Home.vue";
+
+//Routes
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: {
+      title: "Home",
+    },
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
