5233f21c2ccfe345538b956df1bcdc4a905da16a
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-11-18 12:07:14 +0700

    feat: export footer component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -247,85 +247,12 @@
       ></iframe>
     </div>
   </div>
-  <div class="v-vw-100 px-36 py-16 bg-sv-yellow-400 text-sv-black">
-    <div class="grid grid-cols-12 items-center justify-center">
-      <div class="col-span-9">
-        <div class="flex flex-col gap-12 pr-24">
-          <div class="flex items-start text-4xl justify-between font-roboto">
-            <div class="flex flex-col gap-2">
-              <div class="font-semibold font-kanit cursor-pointer">
-                About Us
-              </div>
-            </div>
-            <div class="flex flex-col gap-2">
-              <div class="font-semibold font-kanit cursor-pointer">
-                Services
-              </div>
-              <ul class="list-none text-xl">
-                <li>E-Visa (B211 A)</li>
-                <li>E-Visa (B211 B)</li>
-                <li>KITAS</li>
-                <li>Visa Extention</li>
-                <li>Other Services</li>
-              </ul>
-            </div>
-            <div class="flex flex-col gap-2">
-              <div class="font-semibold font-kanit cursor-pointer">
-                Location
-              </div>
-            </div>
-            <div class="flex flex-col gap-2">
-              <div class="font-semibold font-kanit cursor-pointer">
-                Contact Us
-              </div>
-            </div>
-          </div>
-          <div class="grid grid-cols-2 text-xl font-semibold">
-            <div class="flex flex-col gap-2">
-              <div class="flex items-center gap-2">
-                <span class="fa fa-fw fa-phone"></span>
-                (+62)81238655358
-              </div>
-              <div class="flex items-center gap-2">
-                <span class="fa fa-fw fa-envelope-open-text"></span>
-                smartvisabali1@gmail.com
-              </div>
-            </div>
-            <div class="flex flex-col gap-2">
-              <div class="flex items-start gap-2">
-                <span class="fa fa-fw fa-map-marker-alt"></span>
-                Jl. Tukad Batanghari VI A GBM 1 No.1, Panjer, Kec. Denpasar
-                Bar., Kota Denpasar, Bali 80225
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-span-3 flex flex-col gap-2">
-        <img src="../assets/logo.svg" class="w-full" />
-        <div class="flex items-center text-6xl justify-evenly">
-          <a
-            href="https://www.instagram.com/smartvisa.bali/?hl=en"
-            class="cursor-pointer hover:opacity-90"
-            ><span class="fab fa-instagram"></span
-          ></a>
-          <a href="#" class="cursor-pointer hover:opacity-90"
-            ><span class="fab fa-facebook"></span
-          ></a>
-          <a href="#" class="cursor-pointer hover:opacity-90"
-            ><span class="fab fa-whatsapp"></span
-          ></a>
-        </div>
-      </div>
-      <div class="col-span-12 text-center text-xl">
-        2022 © All rights reserved by Smart Visa Bali
-      </div>
-    </div>
-  </div>
+  <Footer v-bind:bgColor="'bg-sv-yellow-400'"></Footer>
 </template>
 
 <script>
 import Navbar from "../components/Navbar.vue";
+import Footer from "../components/Footer.vue";
 export default {
   setup() {},
   data() {
@@ -338,6 +265,6 @@ export default {
       this.carouselImage = id;
     },
   },
-  components: { Navbar },
+  components: { Navbar, Footer },
 };
 </script>
